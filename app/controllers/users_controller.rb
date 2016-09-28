0563fd0f14f679a0cd3aa7bdf11de8e85923f161
Edmund Wong <wongkokliong99@hotmail.com>
2016-09-28 15:39:19 +0800

    added log in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to the alpha blog #{@user.username}"
+			session[:user_id] = @user.id
 			redirect_to articles_path
 		else
 			flash[:danger] = "There was something wrong with your registration details"
